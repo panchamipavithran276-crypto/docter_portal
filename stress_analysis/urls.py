7157563ae8e183f5486a12172be9c3ccb061893c
vyshnavmv <vyshnavmv15@gmail.com>
2025-10-21 01:36:16 +0530

    new ui - connection google fit

---
+++ stress_analysis/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'stress_analysis'
+
+urlpatterns = [
+    path('dashboard/', views.stress_dashboard, name='dashboard'),
+    path('connect-google-fit/', views.connect_google_fit, name='connect_google_fit'),
+    path('google-fit-callback/', views.google_fit_callback, name='google_fit_callback'),
+    path('disconnect-google-fit/', views.disconnect_google_fit, name='disconnect_google_fit'),
+    path('sync-data/', views.sync_google_fit_data, name='sync_data'),
+    path('api/insights/', views.get_stress_insights, name='get_insights'),
+]
